ff6a483f44321c9a23387dce68a2880d722c5303
Greta Va <greta.vasilevskyte@gmail.com>
2024-01-03 20:01:47 +0200

    Add month navigation func and btns

--- src/Calendar/Calendar.js
+++ src/Calendar/Calendar.js
@@ -1,39 +1,55 @@
 import {
+  addMonths,
   eachDayOfInterval,
   endOfMonth,
   format,
   getDay,
   startOfMonth,
   startOfWeek,
+  subMonths,
 } from 'date-fns';
+import { useState } from 'react';
 import styles from './calendar.module.scss';
 
 export default function Calendar() {
+  const [monthNavigation, setMonthNavigation] = useState(new Date());
   const currentDate = new Date();
   const month = currentDate.toLocaleString('en-AT', { month: 'long' });
   const year = currentDate.getFullYear();
   // const date = currentDate.getDate();
 
   const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const firstDayOfMonth = startOfWeek(startOfMonth(currentDate), {
+  const firstDayOfMonth = startOfWeek(startOfMonth(monthNavigation), {
     weekStartsOn: 1,
   });
-  const lastDayOfMonth = endOfMonth(currentDate);
+  const lastDayOfMonth = endOfMonth(monthNavigation);
   console.log(firstDayOfMonth);
 
   const allDaysInMonth = eachDayOfInterval({
-    start: firstDayOfMonth,
+    start: startOfMonth(monthNavigation),
     end: lastDayOfMonth,
   });
 
-  const startingDayIndex = getDay(firstDayOfMonth);
-  // console.log(result);
+  const startingDayIndex = getDay(startOfMonth(monthNavigation));
+
+  const prevMonth = () => {
+    setMonthNavigation(subMonths(monthNavigation, 1));
+  };
+  const nextMonth = () => {
+    setMonthNavigation(addMonths(monthNavigation, 1));
+  };
 
   return (
     <main className={styles.calendarMain}>
-      <h2>
-        {month} {year}
-      </h2>
+      <section className={styles.monthHeader}>
+        <button onClick={prevMonth} className={styles.navBtn}>
+          Prev
+        </button>
+        <h2>{format(monthNavigation, 'MMMM yyyy')}</h2>
+        <button onClick={nextMonth} className={styles.navBtn}>
+          Next
+        </button>
+      </section>
       <div className={styles.daysOfWeek}>
         {daysOfWeek.map((day) => (
           <div className={styles.dayOfWeek} key={day}>
@@ -41,7 +57,7 @@ export default function Calendar() {
           </div>
         ))}
       </div>
-      <div className={styles.gridNumberDays}>
+      <section className={styles.gridNumberDays}>
         {Array.from({ length: startingDayIndex }).map((_, index) => (
           <div key={`empty-${index}`} />
         ))}
@@ -52,7 +68,7 @@ export default function Calendar() {
             </div>
           );
         })}
-      </div>
+      </section>
     </main>
   );
 }
