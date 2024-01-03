176d9fb79e62417e8debeeddfa06c2775c13b83e
Greta Va <greta.vasilevskyte@gmail.com>
2024-01-03 17:20:31 +0200

    Fix firstDayOfMonth

--- src/Calendar/Calendar.js
+++ src/Calendar/Calendar.js
@@ -1,14 +1,23 @@
-import { eachDayOfInterval, endOfMonth, format, startOfMonth } from 'date-fns';
+import {
+  eachDayOfInterval,
+  endOfMonth,
+  format,
+  getDay,
+  startOfMonth,
+  startOfWeek,
+} from 'date-fns';
 import styles from './calendar.module.scss';
 
 export default function Calendar() {
   const currentDate = new Date();
-  const month = currentDate.toLocaleString('en-US', { month: 'long' });
+  const month = currentDate.toLocaleString('en-AT', { month: 'long' });
   const year = currentDate.getFullYear();
   // const date = currentDate.getDate();
 
-  const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-  const firstDayOfMonth = startOfMonth(currentDate);
+  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const firstDayOfMonth = startOfWeek(startOfMonth(currentDate), {
+    weekStartsOn: 1,
+  });
   const lastDayOfMonth = endOfMonth(currentDate);
   console.log(firstDayOfMonth);
 
@@ -16,6 +25,9 @@ export default function Calendar() {
     start: firstDayOfMonth,
     end: lastDayOfMonth,
   });
+
+  const startingDayIndex = getDay(firstDayOfMonth);
+  // console.log(result);
 
   return (
     <main className={styles.calendarMain}>
@@ -30,6 +42,9 @@ export default function Calendar() {
         ))}
       </div>
       <div className={styles.gridNumberDays}>
+        {Array.from({ length: startingDayIndex }).map((_, index) => (
+          <div key={`empty-${index}`} />
+        ))}
         {allDaysInMonth.map((day, index) => {
           return (
             <div className={styles.gridNumberDay} key={index}>
