667c6bc2dd7144a65183d6143765ea803b259303
Greta Va <greta.vasilevskyte@gmail.com>
2024-01-07 13:19:25 +0100

    Display events using map/filter

--- src/Calendar/Calendar.js
+++ src/Calendar/Calendar.js
@@ -9,9 +9,11 @@ import {
   subMonths,
 } from 'date-fns';
 import { useState } from 'react';
+import { sportData } from '../sportData';
 import styles from './calendar.module.scss';
 
 export default function Calendar() {
+  const [eventList, setEventList] = useState(sportData.data);
   const [monthNavigation, setMonthNavigation] = useState(new Date());
   const currentDate = new Date();
   const month = currentDate.toLocaleString('en-AT', { month: 'long' });
@@ -62,9 +64,30 @@ export default function Calendar() {
           <div key={`empty-${index}`} />
         ))}
         {allDaysInMonth.map((day, index) => {
+          const formattedDate = format(day, 'yyyy-MM-dd');
+          // const eventData = sportData.data.find(
+          //   (event) => event.dateVenue === formattedDate,
+          const eventsOnDay = eventList.filter(
+            (event) => event.dateVenue === formattedDate,
+          );
+          // console.log('Formatted Date:', formattedDate);
+          // console.log(
+          //   'All Date Venues:',
+          //   sportData.data.map((event) => event.dateVenue),
+          // );
+          // console.log('Event Data:', eventData);
+
           return (
             <div className={styles.gridNumberDay} key={index}>
               {format(day, 'd')}
+              {eventsOnDay.map((event, eventIndex) => (
+                <div key={`event-${eventIndex}`}>
+                  <p>
+                    Event: {event.homeTeam?.name} vs {event.awayTeam?.name}
+                  </p>
+                  {/* <p>Time: {event.timeVenueUTC}</p> */}
+                </div>
+              ))}
             </div>
           );
         })}
